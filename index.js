d8dd4f711256cedc0aa81eb4f53065be274364bb
vimnym <113811541+vimnym@users.noreply.github.com>
2022-09-17 21:56:42 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert('En serio?, yo tambien t extraño🙀🙀, yo también me extrañaría UwU')
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('click',function () {
+    alert('Ah no?, yo a ti si XDD🤑')
+});
